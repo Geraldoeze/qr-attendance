b82766c6a353599fffb14d5a9932476be0c32879
Gerald Ezenagu <egerald344@gmail.com>
2023-02-15 15:48:15 +0100

    db connect

--- qr-node/database/mongoConnect.js
+++ qr-node/database/mongoConnect.js
@@ -3,9 +3,9 @@ const MongoClient = mongodb.MongoClient;
 
 let _db;
 
-// const MONGODB_URLINK = `mongodb+srv://${process.env.MONGODB_NAME}:${process.env.MONGODB_PASS}@cluster01.zshtev5.mongodb.net/qr-attendance?retryWrites=true&w=majority`
-// const MONGODB_URL = `mongodb+srv://${process.env.MONGODB_NAME}:${process.env.MONGODB_PASS}@cluster01.zshtev5.mongodb.net/${process.env.MONGODB_DB}?retryWrites=true&w=majority`
-const MONGODB_URL = "mongodb://localhost:27017/qr-attendance";
+
+const MONGODB_URL = `mongodb+srv://${process.env.MONGODB_NAME}:${process.env.MONGODB_PASS}@cluster01.zshtev5.mongodb.net/${process.env.MONGODB_DB}?retryWrites=true&w=majority`
+// const MONGODB_URL = "mongodb://localhost:27017/qr-attendance";
 const mongoConnect = (callback) => {
   MongoClient.connect(MONGODB_URL)
     .then((client) => {
